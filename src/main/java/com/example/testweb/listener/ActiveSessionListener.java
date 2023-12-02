4e4fd49073d716b0356969bf9a2cc86872f6b922
Wilson <hereisleedo@gmail.com>
2023-12-03 07:18:20 +0800

    add listener demo

---
+++ src/main/java/com/example/testweb/listener/ActiveSessionListener.java
@@ -0,0 +1,32 @@
+package com.example.testweb.listener;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import javax.servlet.http.HttpSessionEvent;
+import javax.servlet.http.HttpSessionListener;
+import java.util.concurrent.atomic.AtomicInteger;
+
+public class ActiveSessionListener implements HttpSessionListener {
+
+    private static final Logger log = LoggerFactory.getLogger(ActiveSessionListener.class);
+
+    private final AtomicInteger activeSessions = new AtomicInteger();
+
+    @Override
+    public void sessionCreated(HttpSessionEvent se) {
+        activeSessions.incrementAndGet();
+        log.info("create a new session , active session count:{}" , activeSessions.get());
+    }
+
+    @Override
+    public void sessionDestroyed(HttpSessionEvent se) {
+        activeSessions.decrementAndGet();
+        log.info("session destroyed, active session count:{}" , activeSessions.get());
+
+    }
+
+    public int getActiveSessions() {
+        return activeSessions.get();
+    }
+}
---
+++ src/main/java/com/example/testweb/listener/ActiveSessionListenerConfiguration.java
@@ -0,0 +1,15 @@
+package com.example.testweb.listener;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import javax.servlet.http.HttpSessionListener;
+
+@Configuration
+public class ActiveSessionListenerConfiguration {
+
+    @Bean
+    public HttpSessionListener httpSessionListener() {
+        return new ActiveSessionListener();
+    }
+}
