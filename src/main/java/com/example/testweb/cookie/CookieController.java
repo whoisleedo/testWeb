9194fea1fb6f68e0f680247d397bcc62ca20bc81
Wilson <hereisleedo@gmail.com>
2023-12-02 22:36:46 +0800

    add cookie demo

---
+++ src/main/java/com/example/testweb/cookie/CookieController.java
@@ -0,0 +1,48 @@
+package com.example.testweb.cookie;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import javax.servlet.http.Cookie;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+
+@Controller
+public class CookieController {
+
+    private final static Logger log = LoggerFactory.getLogger(CookieController.class);
+
+    @GetMapping("/settheme")
+    public String setTheme(@RequestParam(value = "theme" , required = false , defaultValue = "default") String theme,
+                           HttpServletResponse response) {
+
+        log.info("setTheme color:{}" , theme);
+        Cookie cookie = new Cookie("theme", theme);
+        cookie.setMaxAge(5 * 60); // 保存5mins
+        response.addCookie(cookie);
+        return "redirect:/gettheme";
+    }
+
+    @GetMapping("/gettheme")
+    public String getTheme(HttpServletRequest request, Model model) {
+        Cookie[] cookies = request.getCookies();
+        if (cookies != null) {
+            for (Cookie cookie : cookies) {
+                if ("theme".equals(cookie.getName())) {
+                    String theme = cookie.getValue();
+                    log.info("getTheme color:{}" , theme);
+                    model.addAttribute("theme", theme);
+                    break;
+                }
+            }
+        }
+        return "showTheme";
+    }
+
+
+}
